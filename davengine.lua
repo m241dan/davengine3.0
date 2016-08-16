95737458905bd2a102a2077295d8c44f58a91e5b
Daniel R. Koris <korisd@gmail.com>
2016-08-16 18:56:57 -0400

    requires in the boot script

--- davengine.lua
+++ davengine.lua
@@ -1,3 +1,16 @@
+----------------------------------
+-- Setup our Modules from /libs --
+----------------------------------
 package.path = package.path .. ";./libs/?.lua"
+package.cpath = package.cpath .. ";./libs/?.so"
 
-Luatils = require( "libs.luautils" )
+Luatils = require( "luautils" )
+Server = require( "server" )
+Client = require( "client" )
+EventQueue = require( "eventqueue" )
+DRoutine = require( "droutine" )
+LuaSha = require( "luasha" )
+DBuffer = require( "dbuffer" )
+DataManager = require( "datamanager" )
+Data = require( "data" )
+Time = require( "time" )
