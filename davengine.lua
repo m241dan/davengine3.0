c8536354c25341e192f14e73759d1b991a2a0b97
Daniel R. Koris <korisd@gmail.com>
2016-08-19 12:34:23 -0400

    added some meat to Davengine.lua, sets up server and accept and poll events

--- davengine.lua
+++ davengine.lua
@@ -7,7 +7,9 @@ package.cpath = package.cpath .. ";./libs/?.so"
 Luatils = require( "luautils" )
 Server = require( "server" )
 Client = require( "client" )
-EventQueue = require( "eventqueue" )
+EventQueue = require( "eventqueue" ) -- cannot be instanced
+Event = EventQueue.event -- a shortcut, could also just do things like EventQueue.event:new()
+Events = require( "events" )
 DRoutine = require( "droutine" )
 LuaSha = require( "luasha" )
 DBuffer = require( "dbuffer" )
@@ -15,3 +17,32 @@ DataManager = require( "datamanager" )
 Data = require( "data" )
 Time = require( "time" )
 LFS = require( "lfs" )
+
+
+SERVER_PORT = 6500
+ACCEPT_INTERVAL = EventQueue.second
+POLL_INTERVAL = EventQueue.second / 10
+
+function main()
+   print( "DavEngine starting..." )
+   bootServer( SERVER_PORT )
+
+   print( "Starting the Event Queue" )
+   EventQueue.run()
+
+   print( "DavEngine shutting down..." )      
+
+end;
+
+function bootServer( port )
+   print( "Booting the TCP Server..." )
+   server = Server:new( port )
+   server:start()
+   server.accept_event = Event:new( Event.newAcceptNew, ACCEPT_INTERVAL, { server }, "Accepting new connections." )
+   server.poll_event = Event:new( Event.newPollEvent, POLL_INTERVAL, { server }, "Polling the Clients connected to the server." ) -- Every 1/10th of a second
+
+   EventQueue.insert( server.accept_event )
+   EventQueue.insert( server.poll_event )
+end;
+
+main()
