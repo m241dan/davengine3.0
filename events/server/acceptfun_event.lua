1c8b11c3acca3b8b8ce85a3609847229bfcf85dd
Daniel R. Koris <korisd@gmail.com>
2016-08-23 23:02:38 -0400

    updated the accept function, working on getting towards something sustainable

--- events/server/acceptfun_event.lua
+++ events/server/acceptfun_event.lua
@@ -2,8 +2,19 @@ return "accept event", function( server )
    local nc
 
    nc = server:accept()
-   if( nc ) then
-      nc:send( "You have connected!" )
+   if( not nc ) then
+      print( "Server event could not accept new connection." )
+      return ACCEPT_INTERVAL
    end
+
+   local new_dm = DataManager:new( nc )
+   local data = Data:new()
+
+   data:setInterp( "interpreters/new_connection.lua" )
+   new_dm:AAS( data );
+   new_dm:setupInterp( data );
+
+   DataManager.dataDump()
+
    return ACCEPT_INTERVAL;
 end;
