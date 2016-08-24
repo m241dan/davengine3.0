11dc0ce3fd13c43887f75e64ddc5138b84e2bc65
Daniel R. Koris <korisd@gmail.com>
2016-08-23 21:50:00 -0400

    updated this to be more autonamous

--- libs/events.lua
+++ libs/events.lua
@@ -1,25 +1,35 @@
 -- TODO: Automate to automatically require all files and a call to it will automatically return the right function
 
-local DRoutine = require( "droutine" )
-
 local E = {}
 
-local pollFunction = dofile( "events/pollfun.lua" )
-local acceptFunction = dofile( "events/acceptfun.lua" )
+local events = {}
 
-function E.newPollEvent( arg )
-   if( arg == "droutine" ) then
-      return DRoutine:new( pollFunction )
-   else
-      return pollFunction
+function E.init( path )
+   --load events from path given, will load all _event.lua files
+   for entry in LFS.dir( path ) do
+      if( entry ~= "." and entry ~= ".." and entry:find( "_event.lua" ) ) then
+         local name, func = dofile( path .. "/" .. entry );
+         if( type( name ) ~= "string" ) then
+            print( "Event file: " .. entry .. " does not return a string as its first argument.\n" )
+            goto next;
+         end
+         if( type( func ) ~= "function" ) then
+            print( "Event file: " .. entry .. " does not return a function as its second argument.\n" )
+            goto next;
+         end
+         events[name] = func; 
+      end
+      ::next::
    end
+
+
 end;
 
-function E.newAcceptEvent( arg )
-   if( arg == "droutine" ) then
-      return DRoutine:new( acceptFunction )
+function E.getEvent( name, drout )
+   if( not drout ) then
+      return events[name]
    else
-      return acceptFunction
+      return DRoutine:new( event[name] )
    end
 end;
 
