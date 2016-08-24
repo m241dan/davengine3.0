5ee2bdb8c4ae22fd55492ff30900a3982f0f64a4
Daniel R. Koris <korisd@gmail.com>
2016-08-24 00:35:49 -0400

    updated the polling event

--- events/server/pollfun_event.lua
+++ events/server/pollfun_event.lua
@@ -1,5 +1,34 @@
+local function process()
+   for i, c in ipairs( server.connections ) do
+      -- (c)onnections are the socket that datamanager refers to, just some lua flex
+      local dm = DataManager.by_socket[c]
+      local current_data = dm.data[dm.index]
+      local interp = dm.interpreter[current_data]
+
+      -- let's examine the inbuffer
+      for _, ln in ipairs( c.inbuf ) do
+         -- process the line through our current data's interpreter
+         interp( ln )
+         -- if it's dead, shut it down
+         if( interp:status() == "dead" ) then
+            print( "Removing Data, it cannot exist without an interpreter" )
+            dm:remData( current_data )
+            goto next_client
+         end
+      end
+     
+      c.inbuf = {}
+      ::next_client::
+   end
+end
+
 return "poll event", function( server )
-   print( "Polling..." )
+   -- poll clients for new input
+   server:poll()
+   -- process the stuff in the inbufs of the clients
+   process()
+   -- push the outbufs
+   server:push()
+
    return POLL_INTERVAL
 end;
-
