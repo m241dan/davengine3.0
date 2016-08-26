12c12d7741db7d4d22065a066685ad4583429279
Daniel R. Koris <korisd@gmail.com>
2016-08-26 14:41:14 -0400

    working on a new design for interpreters

--- interpreters/new_connection.lua
+++ interpreters/new_connection.lua
@@ -1,10 +1,26 @@
+local input, con, dm, d, state
+
+local sub_i = {}
+
+sub_i[1] = function()
+   if( input:lower() == "save" ) then
+      d:save()
+   else
+      d.name = input
+   end
+end;
+
 local function interpreter( datamanager, data )
-   local input
-   local con = datamanager.socket.connection
+   dm = datamanager
+   d = data
+   con = datamanager.socket.connection
    con:send( "\r\nWelcome to DavEngine 3.0\r\n\r\nWhat is  your name?: " )
+   state = 1
    while( 1 ) do
       input = coroutine.yield()
-      con:send( "Your input:\n   " .. input .. "\n" );
+      if( input:lower() == "save" ) then
+         data:save()
+      end
    end
 end
 
