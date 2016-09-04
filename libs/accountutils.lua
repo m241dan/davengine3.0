9f72c87a116a8a9a5680a5387b28b39596a26f72
Daniel R. Koris <korisd@gmail.com>
2016-09-03 23:59:18 -0400

    added the handleTakeOver func

--- libs/accountutils.lua
+++ libs/accountutils.lua
@@ -17,4 +17,27 @@ function A.connectAccount( account, dm )
    dm:setupInterp( account )
 end;
 
+function A.handleTakeOver( account, d, dm, old_dm )
+   if( not old_dm.socket ) then
+      dm.socket.connection:send( "Reconnecting...\r\n" )
+      old_dm.socket = dm.socket;
+      -- essentially, now we just need to "cleanUpNewConnect" ...
+      dm:remData( d ) -- DM will delete itself if it has no data, and it should
+      d:delete()
+   else
+      -- replace the old socket and close
+      DataManager.by_socket[old_dm.socket] = nil
+      old_dm.socket.connection:send( "Your account is being taken over by someone else logging into it...\r\n" )
+      old_dm.socket:close()
+
+      --swap in the new socket
+      old_dm.socket = dm.socket
+      DataManager.by_socket[dm.socket] = old_dm
+      dm.socket.connection:send( "Taking over an already logged in account!\r\n" )
+      -- essentially, now we just need to "cleanUpNewConnect" ...
+      dm:remData( d ) -- DM will delete itself if it has no data, and it should
+      d:delete()
+   end
+end;
+
 return A
