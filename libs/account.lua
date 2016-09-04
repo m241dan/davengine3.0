0b673f5fe8ee3a7bec24a8957c2fc0bf2797edb4
Daniel R. Koris <korisd@gmail.com>
2016-09-03 21:37:23 -0400

    made it so accounts are trackign themselves

--- libs/account.lua
+++ libs/account.lua
@@ -1,5 +1,7 @@
-local A = Data:new()
+local A = Data:raw()
 
+A.all = {}
+A.by_name = {}
 A.__index = A
 A.type = "account"
 A.interp_path = "interpreters/account_interp.lua"
@@ -7,20 +9,52 @@ A.save_loc = "./accounts/"
 A.save_name = "default_account"
 A.level = 1
 
+local wm = { __mode = "kv" }
+setmetatable( A.by_name, wm )
+
 function A:new( name, passwd )
    a = {}
    setmetatable( a, self )
 
-   -- init some defaults
+  -- init some defaults
    a.name = name
    a.save_name = name
    a.passwd = passwd
+
+  -- store in some lib arrays
+   A.all[a] = a
+   A.by_name[name] = a
+
    return a
 end;
 
 function A:serialize()
    return "Account:load( \"" .. self.save_loc .. self.name .. ".lua\" )"
 end;
+
+function A.load( path )
+   local a = dofile( path )
+
+   if( not a ) then
+      print( "Could not load " .. path .. "." )
+      return nil
+   end
+
+   if( not A.by_name[a.name] ) then
+      setmetatable( a, self )
+      A.all[a] = a
+      A.by_name[a.name] = a
+   else
+      print( "An account with that name has already been loaded.\n" )
+      a = nil
+   end
+
+   return a
+end;
+
+function A:delete()
+   A.all[self] = nil
+end
 
 return A
 
